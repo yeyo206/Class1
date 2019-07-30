96b953a111bcb45e4378795c8b6fa320b7b2f9d8
mac <mac@Macs-MBP-5.attlocal.net>
2019-07-29 20:34:32 -0400

    initial commit

---
+++ src/Class/MyLife.java
@@ -0,0 +1,36 @@
+package Class;
+
+public class MyLife {
+
+    int number= 0;
+    int number1=1;
+    String name = "jon";
+    String lastname= "blankinship";
+    String state="florida";
+
+
+    public void House(){
+        System.out.println("florida");
+    }
+    public void Car(){
+        System.out.println("sedan");
+    }
+    public void Wife(){
+        System.out.println("tatiana");
+    }
+    public void Hobbie(){
+        System.out.println("cars");
+    }
+    public void Food(){
+        System.out.println("steak");
+    }
+    public void Name(){
+        System.out.println(name);
+    }
+    public void LastName(){
+        System.out.println(lastname);
+    }
+    public void State(){
+        System.out.println(state);
+    }
+}
---
+++ src/Class/RunMyClass.java
@@ -0,0 +1,14 @@
+package Class;
+
+public class RunMyClass {
+    public static void main(String[] args) {
+        MyLife mylife = new MyLife();
+        mylife.Car();
+        mylife.Food();
+        mylife.Hobbie();
+        mylife.Wife();
+        mylife.House();
+        mylife.Name();
+        mylife.LastName();
+    }
+}
